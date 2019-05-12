a8c8751398a8f62cf3db5ffc0908b27e15ea1286
RoundCat0857 <round0857cat@gmail.com>
2019-05-13 00:01:16 +0900

    方向転換

--- Assets/Script/Kanako.cs
+++ Assets/Script/Kanako.cs
@@ -1,29 +1,40 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Kanako : MonoBehaviour
-{
-  public float speed = 0.1f;
+public class Kanako : MonoBehaviour {
 
-  // Start is called before the first frame update
-  void Start(){
+    public float speed; //プレイヤーの動くスピード
 
-  }
+    private Vector3 Player_pos; //プレイヤーのポジション
+    private float x; //x方向のImputの値
+    private float z; //z方向のInputの値
+    private Rigidbody rigd;
 
-  // Update is called once per frame
-  void Update() {
-    if (Input.GetKey(KeyCode.W)){
-      transform.Translate(transform.forward*speed);
+    void Start()
+    {
+        Player_pos = GetComponent<Transform>().position; //最初の時点でのプレイヤーのポジションを取得
+        rigd = GetComponent<Rigidbody>(); //プレイヤーのRigidbodyを取得
     }
-    if (Input.GetKey(KeyCode.S)){
-      transform.Translate(-transform.forward*speed);
+
+    void Update()
+    {
+
+        x = Input.GetAxis("Horizontal"); //x方向のInputの値を取得
+        z = Input.GetAxis("Vertical"); //z方向のInputの値を取得
+
+        rigd.velocity = new Vector3(x * speed, 0, z * speed); //プレイヤーのRigidbodyに対してInputにspeedを掛けた値で更新し移動
+
+        Vector3 diff = transform.position - Player_pos; //プレイヤーがどの方向に進んでいるかがわかるように、初期位置と現在地の座標差分を取得
+
+        if (diff.magnitude > 0.01f) //ベクトルの長さが0.01fより大きい場合にプレイヤーの向きを変える処理を入れる(0では入れないので）
+        {
+            transform.rotation = Quaternion.LookRotation(diff);  //ベクトルの情報をQuaternion.LookRotationに引き渡し回転量を取得しプレイヤーを回転させる
+        }
+
+        Player_pos = transform.position; //プレイヤーの位置を更新
+
+
     }
-    if (Input.GetKey(KeyCode.D)){
-      transform.Translate(transform.right*speed);
-    }
-    if (Input.GetKey(KeyCode.A)){
-      transform.Translate(-transform.right*speed);
-    }
-  }
+
 }
