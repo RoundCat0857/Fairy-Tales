5b7d50190c1b0c2b05620343a5a127c421538492
Si <bokega>
2019-06-15 02:35:15 +0900

    廊下の壁修正

---
+++ Assets/Project/Script/KanakoRoom/MoveCorridor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MoveCorridor : MonoBehaviour
+{
+
+    void OnTriggerStay()
+    {
+      if(Input.GetMouseButtonDown(0))
+      SceneManager.LoadScene ("Corridor");
+    }
+
+}
