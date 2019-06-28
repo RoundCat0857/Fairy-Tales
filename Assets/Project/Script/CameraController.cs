3a5757249e04a1d5e3148bcf40aac14d0af54698
RoundCat0857 <round0857cat@gmail.com>
2019-06-28 23:59:38 +0900

    コメントを削除しました

--- Assets/Project/Script/CameraController.cs
+++ Assets/Project/Script/CameraController.cs
@@ -11,13 +11,11 @@ public class CameraController : MonoBehaviour {
   }
 
   void Update() {
-      // targetの移動量分、自分（カメラ）も移動する
       transform.position += targetObj.transform.position - targetPos;
       targetPos = targetObj.transform.position;
-
       float mouseInputX = Input.GetAxis("Mouse X");
       float mouseInputY = Input.GetAxis("Mouse Y");
       transform.RotateAround(targetPos, Vector3.up, mouseInputX * Time.deltaTime * 200f);
-      transform.RotateAround(targetPos, transform.right, mouseInputY * Time.deltaTime * 100f);
+      // transform.RotateAround(targetPos, transform.right, mouseInputY * Time.deltaTime * 100f);
   }
 }
